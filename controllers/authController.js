5ed40dfd3d87b2a215c0f52b5e8a14bd4a2b1856
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-03-19 11:27:12 -0700

    update

---
+++ controllers/authController.js
@@ -0,0 +1,9 @@
+const login = (req, res) => {
+  res.send("This is the login function");
+};
+
+const register = (req, res) => {
+  res.send("This is the register function");
+};
+
+module.exports = { login, register };
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Routers
+const authRouter = require("./routes/authRouter");
+
+app.use("/api/auth", authRouter);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
