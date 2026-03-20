8bf58cafa8610add23aadf5c1957c3b0a2fbaf45
Dj <148027370+djsy104@users.noreply.github.com>
2026-03-20 09:37:53 -0700

    feat: added user model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,23 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
+
+const User = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name."],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please provide a email."],
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+    minLength: [4, "Password length must be at least 4 characters"],
+  },
+});
